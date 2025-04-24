f475014262ccb815dad4f704b9000183348b00f6
sm227 <hamsm3184@gmail.com>
2025-04-24 12:16:35 +0900

    cors 수정

--- src/main/java/org/example/travellight/config/WebConfig.java
+++ src/main/java/org/example/travellight/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173","http://52.78.233.147:5174")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false)
